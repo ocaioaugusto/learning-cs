3a07450e6f96f766e434e9afa6e3e67d3dcdb8df
caio <caioag42@gmail.com>
2020-07-07 10:18:55 -0300

    adding book class

---
+++ LearningC/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningC
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<double> grades;
+
+        internal void ShowStatistics()
+        {
+            throw new NotImplementedException();
+        }
+        private string name;
+    }
+}
