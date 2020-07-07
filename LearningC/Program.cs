ab1a7c573705cf00d3190f93932fe9063a1a6c55
caio <caioag42@gmail.com>
2020-07-07 09:42:27 -0300

    Initial check-in of module LearningC

---
+++ LearningC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningC
+{
+    class Program
+    {
+        public static char input { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            var book = new Book();
+            while (input != 'q')
+            {
+                Console.WriteLine("digite...");
+                var input = Console.ReadLine();
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }
+
+
+            book.ShowStatistics();
+        }
+    }
+}
